96593588e9baaeb635e8d005296ea2d8fa3ef3a1
Augustin Viard <gugusviard@gmail.com>
2025-04-13 18:11:25 +0200

    import name error fix

--- src/components/galleryElement.jsx
+++ src/components/galleryElement.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import "../style/components/GalleryElement.scss";
+import "../style/components/galleryElement.scss";
 
 function GalleryElement({ cover, title, id }) {
     return (
