67abb471a4d7a48cde5ad9ea66c4f2c4831bb44b
Augustin Viard <gugusviard@gmail.com>
2025-04-18 11:37:06 +0200

    trying with hash router

--- src/root.jsx
+++ src/root.jsx
@@ -11,13 +11,14 @@ import data from './assets/data.json'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename='/kasa_project/'>
+        <BrowserRouter basename='/kasa_project'>
             <Layout>
-                <Routes path="/kasa_project">
+                <Routes>
                     <Route path="/" element={<Home data={data} />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/logement" element={<Logement data={data}/>} />
-                    <Route path="*" element={<NotFound />} />
+                    <Route path="/not-found" element={<NotFound />} />
+                    <Route path="*" element={<Navigate replace to="/not-found" />} />
                 </Routes>
             </Layout>
         </BrowserRouter>
